e6e076042b4d4111f2c6a1832428b6fd583d884a
Maciej Kucharczyk <maciej.kucharczyk@hotmail.com>
2025-05-02 18:45:53 +0200

    Group kinds of issues

---
+++ core/src/main/java/eu/softpol/lib/nullaudit/core/report/Category.java
@@ -0,0 +1,7 @@
+package eu.softpol.lib.nullaudit.core.report;
+
+public enum Category {
+  VERIFY_JSPECIFY_ANNOTATIONS,
+  REQUIRE_NULL_MARKED,
+  REQUIRE_SPECIFIED_NULLNESS
+}
--- core/src/main/java/eu/softpol/lib/nullaudit/core/report/Kind.java
+++ core/src/main/java/eu/softpol/lib/nullaudit/core/report/Kind.java
@@ -1,8 +1,18 @@
 package eu.softpol.lib.nullaudit.core.report;
 
 public enum Kind {
-  UNSPECIFIED_NULLNESS,
-  MISSING_NULL_MARKED_ANNOTATION,
-  INVALID_NULL_MARK_COMBINATION,
-  INVALID_NULLNESS_ON_PRIMITIVE
+  UNSPECIFIED_NULLNESS(Category.REQUIRE_SPECIFIED_NULLNESS),
+  MISSING_NULL_MARKED_ANNOTATION(Category.REQUIRE_NULL_MARKED),
+  INVALID_NULL_MARK_COMBINATION(Category.VERIFY_JSPECIFY_ANNOTATIONS),
+  INVALID_NULLNESS_ON_PRIMITIVE(Category.VERIFY_JSPECIFY_ANNOTATIONS);
+
+  private final Category category;
+
+  Kind(Category category) {
+    this.category = category;
+  }
+
+  public Category getCategory() {
+    return category;
+  }
 }

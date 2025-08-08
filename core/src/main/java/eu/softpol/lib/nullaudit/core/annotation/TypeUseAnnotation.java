52e1947be11861ba69b086d7143a11cd8a43df75
Maciej Kucharczyk <maciej.kucharczyk@hotmail.com>
2025-08-08 23:23:27 +0200

    Update TypeUseAnnotation

--- core/src/main/java/eu/softpol/lib/nullaudit/core/annotation/TypeUseAnnotation.java
+++ core/src/main/java/eu/softpol/lib/nullaudit/core/annotation/TypeUseAnnotation.java
@@ -1,19 +1,20 @@
 package eu.softpol.lib.nullaudit.core.annotation;
 
 import eu.softpol.lib.nullaudit.core.analyzer.visitor.ClassUtil;
+import eu.softpol.lib.nullaudit.core.model.NAAnnotation;
 
-public record TypeUseAnnotation(String clazz) {
+public record TypeUseAnnotation(String fqcn) {
 
   public static final TypeUseAnnotation JSPECIFY_NULLABLE =
-      ofClazz("org.jspecify.annotations.Nullable");
+      of(NAAnnotation.NULLABLE.fqcn());
   public static final TypeUseAnnotation JSPECIFY_NON_NULL =
-      ofClazz("org.jspecify.annotations.NonNull");
+      of(NAAnnotation.NON_NULL.fqcn());
 
   public static TypeUseAnnotation ofDescriptor(String descriptor) {
-    return ofClazz(ClassUtil.getClassNameFromDescriptor(descriptor));
+    return of(ClassUtil.getClassNameFromDescriptor(descriptor));
   }
 
-  public static TypeUseAnnotation ofClazz(String clazz) {
-    return new TypeUseAnnotation(clazz);
+  public static TypeUseAnnotation of(String fqcn) {
+    return new TypeUseAnnotation(fqcn);
   }
 }
--- core/src/test/java/eu/softpol/lib/nullaudit/coretest/annotation/TypeUseAnnotationTest.java
+++ core/src/test/java/eu/softpol/lib/nullaudit/coretest/annotation/TypeUseAnnotationTest.java
@@ -3,20 +3,15 @@ package eu.softpol.lib.nullaudit.coretest.annotation;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import eu.softpol.lib.nullaudit.core.annotation.TypeUseAnnotation;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.api.Test;
 
-@Disabled
 class TypeUseAnnotationTest {
 
-  @ParameterizedTest
-  @CsvSource(value = {
-      "Lorg/jspecify/annotations/Nullable;, org.jspecify.annotations.Nullable",
-      "Lorg/jspecify/annotations/NonNull;,  org.jspecify.annotations.NonNull",
-  })
-  void test(String descriptor, String clazz) {
-    assertThat(TypeUseAnnotation.ofDescriptor(descriptor))
-        .isEqualTo(TypeUseAnnotation.ofClazz(clazz));
+  @Test
+  void test() {
+    assertThat(TypeUseAnnotation.ofDescriptor("Lorg/jspecify/annotations/Nullable;"))
+        .isEqualTo(TypeUseAnnotation.JSPECIFY_NULLABLE);
+    assertThat(TypeUseAnnotation.ofDescriptor("Lorg/jspecify/annotations/NonNull;"))
+        .isEqualTo(TypeUseAnnotation.JSPECIFY_NON_NULL);
   }
 }
